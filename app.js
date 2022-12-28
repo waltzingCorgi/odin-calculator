88bfdbb9e83840feeaf13f2996216852b497b449
Patrick <16686610+waltzingCorgi@users.noreply.github.com>
2022-12-27 21:18:13 -0600

    Add keyboard support

--- app.js
+++ app.js
@@ -11,6 +11,72 @@ const cButton = document.querySelector(".clear");
 const acButton = document.querySelector(".ac");
 const calculateButton = document.querySelector(".calculate");
 
+// Button input bindings
+numButtons.forEach(button => button.addEventListener("click", () => inputNumber(button.dataset.value)));
+decimalButton.addEventListener("click", () => inputDecimal());
+operatorButtons.forEach(button => button.addEventListener("click", () => inputOperator(button.dataset.value)));
+calculateButton.addEventListener("click", () => inputCalculate());
+cButton.addEventListener("click", () => inputClear());
+acButton.addEventListener("click", () => inputAllClear());
+
+// Keyboard bindings
+window.addEventListener("keydown", (e) => {
+    if (/\d/.test(e.key)) inputNumber(e.key);
+    else if (e.key === ".") inputDecimal();
+    else if (isOperator(e.key)) inputOperator(e.key);
+    else if (e.key === "Enter") inputCalculate(e.key);
+    else if (e.key === "c") inputClear();
+    else if (e.key === "C") inputAllClear();
+});
+
+// Input functions
+function inputNumber(value) {
+    const index = getNumIndex();
+
+    numArray[index] += value;
+    output.textContent = numArray[index];
+}
+
+function inputDecimal() {
+    const index = getNumIndex();
+
+    if (numArray[index].includes(".")) return;
+    
+    numArray[index] += ".";
+    if (numArray[index] === ".") numArray[index] = "0" + numArray[index];
+
+    output.textContent = numArray[index];
+}
+
+function inputOperator(value) {
+    const index = getNumIndex();
+
+    if (index === 0) operator = getOperatorFunction(value);
+}
+
+function inputCalculate() {
+    const num1 = Number(numArray[0]);
+    const num2 = Number(numArray[1]);;
+
+    if (getNumIndex() === 1) {
+        output.textContent = operate(operator, num1, num2);
+        reset();
+    }
+}
+
+function inputClear() {
+    const index = getNumIndex();
+
+    numArray[index] = "";
+    output.textContent = numArray[0];
+}
+
+function inputAllClear() {
+    reset();
+
+    output.textContent = numArray[0];
+}
+
 // Operator functions
 function add(num1, num2) {
     return num1 + num2;
@@ -32,61 +98,13 @@ function operate(operator, num1, num2) {
     return operator(num1, num2);
 }
 
-// Button bindings
-numButtons.forEach(button => button.addEventListener("click", () => {
-    const index = getNumIndex();
-    console.log(index);
-
-    numArray[index] += button.dataset.value;
-    console.log(button.dataset.value);
-    output.textContent = numArray[index];
-}));
-
-operatorButtons.forEach(button => button.addEventListener("click", () => {
-    const index = getNumIndex();
-
-    if (index === 0) {
-        operator = getOperatorFunction(button.dataset.value);
-        console.log(operator);
-    }
-}));
-
-calculateButton.addEventListener("click", () => {
-    const num1 = Number(numArray[0]);
-    const num2 = Number(numArray[1]);;
-
-    if (getNumIndex() === 1) {
-        output.textContent = operate(operator, num1, num2);
-        reset();
-    }
-});
-
-decimalButton.addEventListener("click", () => {
-    const index = getNumIndex();
-
-    if (numArray[index].includes(".")) return;
-    
-    numArray[index] += ".";
-    if (numArray[index] === ".") numArray[index] = "0" + numArray[index];
-
-    output.textContent = numArray[index];
-});
-
-cButton.addEventListener("click", () => {
-    const index = getNumIndex();
-
-    numArray[index] = "";
-    output.textContent = numArray[index];
-});
-
-acButton.addEventListener("click", () => {
-    reset();
-
-    output.textContent = numArray[0];
-})
-
+// Helper functions
 function getNumIndex() {
     return (operator) ? 1 : 0;
+}
+
+function isOperator(key) {
+    return (key === "+") || (key === "-") || (key === "*") || (key === "/");
 }
 
 function getOperatorFunction(operator) {
@@ -109,3 +127,13 @@ function reset() {
     numArray[0] = "";
     numArray[1] = "";
 }
+
+// Function graveyard
+// function inputNumberFactory(value) {
+//     return function inputNumber(e) {
+//         const index = getNumIndex();
+
+//         numArray[index] += value;
+//         output.textContent = numArray[index];
+//     }
+// }
