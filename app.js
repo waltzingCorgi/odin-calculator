640c228448199acf183609d02017bbf002520b69
Patrick <16686610+waltzingCorgi@users.noreply.github.com>
2022-12-29 20:56:50 -0600

    Add backspace and inverse

--- app.js
+++ app.js
@@ -8,31 +8,62 @@ const output = document.querySelector(".current-output");
 const expressionDisplay = document.querySelector(".expression");
 const numButtons = document.querySelectorAll(".num");
 const decimalButton = document.querySelector(".decimal");
+const inverseButton = document.querySelector(".inverse");
 const operatorButtons = document.querySelectorAll(".operator");
+const backspaceButton = document.querySelector(".backspace");
 const cButton = document.querySelector(".clear");
 const acButton = document.querySelector(".ac");
 const calculateButton = document.querySelector(".calculate");
 
 // Button input bindings
-numButtons.forEach(button => button.addEventListener("click", () => inputNumber(button.dataset.value)));
-decimalButton.addEventListener("click", () => inputDecimal());
-operatorButtons.forEach(button => button.addEventListener("click", () => inputOperator(button.dataset.value)));
-calculateButton.addEventListener("click", () => inputCalculate());
-cButton.addEventListener("click", () => inputClear());
-acButton.addEventListener("click", () => inputAllClear());
+numButtons.forEach(button => button.addEventListener("click", () => {
+    inputNumber(button.dataset.value);
+    button.blur();
+}));
+decimalButton.addEventListener("click", () => {
+    inputDecimal();
+    decimalButton.blur();
+});
+inverseButton.addEventListener("click", () => {
+    inputInverse();
+    inverseButton.blur();
+})
+operatorButtons.forEach(button => button.addEventListener("click", () => {
+    inputOperator(button.dataset.value);
+    button.blur();
+}));
+calculateButton.addEventListener("click", () => {
+    inputCalculate()
+    calculateButton.blur();
+});
+backspaceButton.addEventListener("click", () => {
+    inputBackspace();
+    backspaceButton.blur();
+})
+cButton.addEventListener("click", () => {
+    inputClear();
+    cButton.blur();
+});
+acButton.addEventListener("click", () => {
+    inputAllClear();
+    acButton.blur();
+});
 
 // Keyboard bindings
 window.addEventListener("keydown", (e) => {
     if (/\d/.test(e.key)) inputNumber(e.key);
     else if (e.key === ".") inputDecimal();
+    else if (e.key === "r") inputInverse();
     else if (isOperator(e.key)) inputOperator(e.key);
     else if (e.key === "Enter") inputCalculate(e.key);
+    else if (e.key === "Backspace") inputBackspace();
     else if (e.key === "c") inputClear();
     else if (e.key === "C") inputAllClear();
 });
 
 // Input functions
 function inputNumber(value) {
+    result = "";
     const index = getNumIndex();
 
     numArray[index] += value;
@@ -40,6 +71,7 @@ function inputNumber(value) {
 }
 
 function inputDecimal() {
+    result = "";
     const index = getNumIndex();
 
     if (numArray[index].includes(".")) return;
@@ -50,15 +82,39 @@ function inputDecimal() {
     updateDisplay(index);
 }
 
-function inputOperator(value) {
+function inputInverse() {
+    result = "";
     const index = getNumIndex();
 
-    if (index === 0) {
-        if (numArray[index] == "") numArray[index] = 0;
-        operator = getOperatorFunction(value);
+    if (Number(numArray[index]) === 0) {
+        updateDisplay(index);
+        return;
+    };
+    
+    if (numArray[index].substring(0,1) === "-") {
+        numArray[index] = numArray[index].substring(1, numArray[index].length);
+    } else {
+        numArray[index] = "-" + numArray[index];
     }
 
     updateDisplay(index);
+}
+
+function inputOperator(value) {
+    const index = getNumIndex();
+
+    if ((index === 0) & (result == "")) {
+        if (numArray[0] == "") numArray[0] = 0;
+        operator = getOperatorFunction(value);
+    } else if ((index === 0) & (result !== "")) {
+        inputCalculate();
+        numArray[0] = result;
+        result = "";
+        operator = getOperatorFunction(value);
+    } else {
+        operator = getOperatorFunction(value);
+    }
+    updateDisplay(0);
 }
 
 function inputCalculate() {
@@ -73,7 +129,17 @@ function inputCalculate() {
     }
 }
 
+function inputBackspace() {
+    result = "";
+    const index = getNumIndex();
+    const length = numArray[index].length;
+
+    if (length > 0) numArray[index] = numArray[index].substring(0, length - 1);
+    updateDisplay(index);
+}
+
 function inputClear() {
+    result = "";
     const index = getNumIndex();
 
     numArray[index] = "";
@@ -81,6 +147,7 @@ function inputClear() {
 }
 
 function inputAllClear() {
+    result = "";
     reset();
     updateDisplay(0);
 }
@@ -160,7 +227,6 @@ function getEqualsSymbol(result) {
 function reset() {
     numArray[0] = "";
     numArray[1] = "";
-    result = "";
     operator = "";
 }
 
